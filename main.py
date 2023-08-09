af5cbdb507a397944dce38c6fb3bc96256aac66f
jesseturner21 <jesseturner2021@gmail.com>
2023-08-09 14:50:44 -0700

    Created html and css to make the website functional with flask submission of values works with a single place and price

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-
-from flask import Flask
+from flask import Flask, render_template, request
 from database import Database
 from flight_checker import FlightChecker
 
 # -------------------Flask------------------#
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='/static')
 
 # -------------------CONTROL------------------#
 fc = FlightChecker()
-
 db = Database()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    print('in hello')
+    if request.method == 'POST':
+        print('in request')
+        email = request.form.get('email')
+        fly_from = request.form.get('fly_from')
+        fly_to = request.form.get('fly_to')
+        price_to = request.form.get('price')
+        date_from = request.form.get('date_from')
+        date_to = request.form.get('date_to')
+        return_from = request.form.get('return_from')
+        return_to = request.form.get('return_to')
+        # Add user to database
+        code = fc.get_codes([fly_from, fly_to])
+        db.add_user(email=email, fly_from=code[0], fly_to=[code[1]], date_from=date_from,
+                    date_to=date_to, return_from=return_from, return_to=return_to, price_to=[price_to])
+
+        return render_template("submitted.html")
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
